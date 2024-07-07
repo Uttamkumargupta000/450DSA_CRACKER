0ae8b87072f7297139ee40bcdc06e3de032382d2
Uttamkumargupta000 <uttamkumargupta000@gmail.com>
2024-07-07 22:56:09 +0530

    additional_Method_added

--- ARRAY/03_Kth_Element_Array.cpp
+++ ARRAY/03_Kth_Element_Array.cpp
@@ -19,7 +19,34 @@ class Solution{
 
 // ########## Approach 2 ###########
 
-//     TC = O(n*log(n));
+//     TC = O(k)
+
+//using Heap or Priority Queue 
+
+class Solution{
+    public:
+    // arr : given array
+    // l : starting index of the array i.e 0
+    // r : ending index of the array i.e size-1
+    // k : find kth smallest element and return using this function
+    int kthSmallest(int arr[], int l, int r, int k) {
+        //sort the array 
+        priority_queue<int> pq;
+        
+        for(int i=0; i<k; i++){
+            pq.push(arr[i]);
+        }
+        
+        for(int i=k; i<=r; i++){
+            if(arr[i] < pq.top()){
+                pq.pop();
+                pq.push(arr[i]);
+            }
+        }
+        
+        return pq.top();
+    }
+};
 
 
 
