9272e280d214fd9a6b20d25c2d0a33a4514361f3
Uttamkumargupta000 <uttamkumargupta000@gmail.com>
2024-07-10 23:47:31 +0530

    Median_in_rowWise_matrix

---
+++ MATRIX/03_Median_inRowwise_matrix.cpp
@@ -0,0 +1,31 @@
+//--------- APPROACH 1 - SIMPLE ONE --------
+
+// TC = O(N^2
+)
+
+class Solution{   
+public:
+    int median(vector<vector<int>> &matrix, int R, int C){
+        // code here    
+        vector<int> ans;
+        
+        for(int i = 0; i < R; i++){
+            for(int j = 0; j < C; j++){
+                ans.push_back(matrix[i][j]);
+            }
+        }
+        
+        sort(ans.begin(), ans.end());
+        
+        int n = ans.size();
+        if(n % 2 == 0){
+            return (ans[n/2] + ans[(n-1)/2])/2;
+        }
+        else{
+            return ans[n/2];
+        }
+    }
+};
+
+
+// ------------ APPROACH 2 OPTIMISED ONE -------------
