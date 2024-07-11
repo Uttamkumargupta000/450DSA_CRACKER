fda699eac60052b3ec3a515291fd762e71d0c3a5
Uttamkumargupta000 <uttamkumargupta000@gmail.com>
2024-07-11 14:38:27 +0530

    Another appraoched added

--- MATRIX/03_Median_inRowwise_matrix.cpp
+++ MATRIX/03_Median_inRowwise_matrix.cpp
@@ -29,3 +29,55 @@ public:
 
 
 // ------------ APPROACH 2 OPTIMISED ONE -------------
+
+// TC = O(R* LOG (C))
+
+// USING BINARY SEARCH AND UPPER BOUND STL FUNCTION
+
+
+class Solution{
+    //count function less or equal to middle element
+    private:
+    int countLessEqual(vector<int> &row, int mid){
+        return upper_bound(row.begin(), row.end(), mid) - row.begin();
+    }
+public:
+    int median(vector<vector<int>> &matrix, int R, int C){
+        R = matrix.size();
+        C = matrix[0].size();
+        
+        //finging the min and max element in row wise sorted 
+        int minEle = matrix[0][0];
+        int maxEle = matrix[0][C-1];
+        
+        //taking the min and max from the matrix; 
+        for(int i = 0 ; i < R; i++){
+            minEle = min(minEle, matrix[i][0]);
+            maxEle = max(maxEle, matrix[i][C-1]);
+        }
+        
+        //using the binary search for the remaining element 
+        int desired = (R*C + 1)/2;
+        
+        while(minEle < maxEle){
+            
+            int mid = minEle + (maxEle - minEle)/2;
+            
+            int count = 0;
+            
+            for(int i = 0; i < R ; i++){
+                //count function for less or equal middle element 
+                count += countLessEqual(matrix[i], mid);
+            }
+            
+            // adjusting the search the range
+            if(count < desired){
+                minEle = mid + 1;
+            }
+            else{
+                maxEle = mid;
+            }
+        }
+        return minEle;
+    }
+};
