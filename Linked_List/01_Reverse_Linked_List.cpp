d0f804018b4c6f00f19f5f9de273b942189d947f
Uttamkumargupta000 <uttamkumargupta000@gmail.com>
2024-08-19 10:45:31 +0530

    Reverse_LInke_List

---
+++ Linked_List/01_Reverse_Linked_List.cpp
@@ -0,0 +1,46 @@
+// ------- Appraoch 1 using iteration --------------
+
+
+//  TC = O(N)
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head == NULL){
+            return NULL;
+        }
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* forward = NULL;
+        
+        while(curr!= NULL){
+            forward = curr -> next;
+            curr -> next = prev;
+            prev = curr;
+            curr = forward;
+        }
+        return prev;
+    }
+};
+
+
+
+// -------------- Approach 2 using Recursion ---------------
+
+
+//  TC = O(N)
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        
+        if(head == NULL || head -> next == NULL){
+            return head;
+        }
+
+        ListNode* smallerNode = reverseList(head -> next);
+        head -> next -> next = head;
+        head -> next = NULL;
+        return smallerNode;
+
+    }
+};
