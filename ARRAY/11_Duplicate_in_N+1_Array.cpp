3e252d7b122e0516322a72399e1bc03d06d226e9
Uttamkumargupta000 <uttamkumargupta000@gmail.com>
2025-01-17 13:18:26 +0530

    Duplicate_in_N+1_array

---
+++ ARRAY/11_Duplicate_in_N+1_Array.cpp
@@ -0,0 +1,22 @@
+// iterative approach or brute force approach
+
+// tc = O(nlogn);
+
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+
+        for(int i = 0 ; i< nums.size(); i++){
+            if(nums[i] == nums[i+1]){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+};
+
+
+//optmised approach
