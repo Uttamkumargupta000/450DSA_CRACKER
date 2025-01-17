de21c37021d2ef98b212c1725f0a3492ba4faaab
Uttamkumargupta000 <uttamkumargupta000@gmail.com>
2025-01-17 12:29:54 +0530

    Optimiese_approach_added

--- ARRAY/08_kadana's_Algorithmn.cpp
+++ ARRAY/08_kadana's_Algorithmn.cpp
@@ -23,4 +23,28 @@ class Solution {
     }
 };
 
-//
+// optimised Approach with maxi always be positive and make sure if currSum goes to negative it will directly be become zero
+
+// tc = O(n)
+
+
+class Solution {
+  public:
+    // Function to find the sum of contiguous subarray with maximum sum.
+    int maxSubarraySum(vector<int> &arr) {
+        // code here...
+        int n = arr.size();
+        int maxi = INT_MIN;
+        int currSum = 0;
+        for(int i = 0; i<n; i++){
+            currSum += arr[i];
+            
+            maxi = max(maxi, currSum);
+            
+            if(currSum < 0){
+                currSum = 0;
+            }
+        }
+        return maxi;
+    }
+};
